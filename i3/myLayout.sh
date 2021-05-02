fee68ad07737108015da2ea1cfc2b3ee9e297073
Mikael <mikael.hatanpaa@protonmail.com>
2021-05-02 21:10:48 +0200

    auto commit backup point

--- i3/myLayout.sh
+++ i3/myLayout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Put this file into ~/.screenlayout/ and exec it in the i3 config
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 760x1440 --rotate normal --dpi 112
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 3440x291 --rotate normal --dpi 112
 xrandr --output DP1 --mode 3440x1440 --pos 0x0 --rotate normal
